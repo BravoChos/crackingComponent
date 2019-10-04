d8f235e4d4bb4deca3c2815d46eb7bf7c09285c0
Song Cho <deeplearner507@gmail.com>
2019-10-04 23:57:25 +0900

    step 4 uncontrolled_optimized

---
+++ uncontrolled_optimized/src/Input.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Input extends React.Component {
+  renderCount = 0;
+  state = {
+    value: ''
+  }
+
+  render() {
+    const { type, placeholder } = this.props
+    const { value } = this.state
+    console.log(placeholder, 'Rendered', ++this.renderCount)
+
+    return <input type={type} placeholder={placeholder} value={value} onChange={this.handleChange} />
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value }, () => {
+      this.props.onChange(this.state.value)
+    })
+  }
+}
+
+export default Input
---
+++ uncontrolled_optimized/src/JoinForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Input from './Input'
+
+class JoinForm extends React.Component {
+  email = ''
+  password = ''
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Input
+          type="email"
+          placeholder="이메일"
+          onChange={this.handleChangeEmail}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          onChange={this.handleChangePassword}
+        />
+        <button type="submit">가입하기</button>
+        <button type="button" onClick={this.handleReset}>초기화</button>
+      </form>
+    );
+  }
+
+  handleChangeEmail = (value) => {
+    this.email = value
+  }
+  
+  handleChangePassword = (value) => {
+    this.password = value
+  }
+
+  handleSubmit = () => {
+    console.log(this.email, this.password)
+  }
+
+  handleReset = () => {
+    this.email = ''
+    this.password = ''
+  }
+}
+
+export default JoinForm
